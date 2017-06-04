52d3ac2269afbe0e28471312e99e3500e956c309
Hamza Jerbi <hamzon.jb@gmail.com>
2017-06-04 03:53:49 +0200

    add maps component

--- src/components/maps.js
+++ src/components/maps.js
@@ -7,17 +7,64 @@ import {
   View
 } from 'react-native';
 
+import MapView from 'react-native-maps'
+
 class maps extends Component {
   render () {
     return (
       <View style={styles.container}>
-        <Text style={styles.title}>Soy el componente Heroes</Text>
+        <MapView
+        style = {styles.map}
+    initialRegion={{
+      latitude: 37.78825,
+      longitude: -122.4324,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421,
+    }}
+  >
+  <MapView.Marker
+
+  coordinate ={{
+         
+    latitude: 37.78825,
+    longitude: -122.4324,
+  }}>
+    <View style ={styles.radius}>
+      <View style ={styles.marker} />
+      </View>
+
+
+  
+  </MapView.Marker>
+
+  </MapView>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
+  radius: {
+    height: 50,
+    width :50,
+    borderRadius: 50 / 2,
+    overflow : 'hidden',
+    borderWidth: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  marker:{
+     height: 20,
+    width :20,
+    borderRadius: 20 / 2,
+    overflow : 'hidden',
+    borderWidth: 1,
+    alignItems: 'center',
+        backgroundColor :'#007AFF',
+    justifyContent: 'center'
+
+  },
   container: {
     flex: 1,
     width: null,
@@ -25,6 +72,13 @@ const styles = StyleSheet.create({
     alignItems: 'stretch',
     padding: 30
 
+  },
+  map :{
+    left :0,
+    right: 0,
+    top: 0,
+    bottom :0,
+    position : 'absolute'
   },
   title: {
     marginTop: 40,
